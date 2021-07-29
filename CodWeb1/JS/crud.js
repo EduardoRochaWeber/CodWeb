d5b44676ac7f2911dbbd4635332e236604ad7a21
Eduardo Weber <edu.weber07@gmail.com>
2021-07-29 14:21:20 -0300

    Lista funcional

--- CodWeb1/JS/crud.js
+++ CodWeb1/JS/crud.js
@@ -6,7 +6,8 @@ function salvar(){
     let descricao = document.getElementById("descricao").value;
     cat = {"id":id, "nome":nome, "descricao":descricao};
     categorias.push(cat);
-    mostraTabela();
+    localStorage.setItem("categorias", JSON.stringify(categorias));
+    mostraStorage();
 }
 function mostraTabela(){
     let tabela = document.getElementById("categorias");
@@ -16,6 +17,19 @@ function mostraTabela(){
         corpo.innerHTML += `<tr><td>${c["id"]}</td><td>${c["nome"]}</td><td>${c["descricao"]}</td></tr>`;
     });
 }
+function mostraStorage(){
+    categorias = JSON.parse(localStorage.getItem("categorias"));
+}
 window.onload = function(){
+    mostraStorage();
     mostraTabela();
 }
+/*function deletar(id){
+    let deletar = 
+    categorias = JSON.parse(localStorage.getItem("categorias"));
+    mostraTabela();
+}
+function editar(id){
+    let editar = categorias.find("id");
+    categorias[0].id = 
+}*/
